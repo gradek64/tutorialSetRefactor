f765bcd3dd312dee34cac1e9dcd82f3332e48199
GILGRZ <gradek64@yahoo.com>
2018-12-08 12:40:59 +0000

    side menu started

--- routes/index.js
+++ routes/index.js
@@ -1,51 +1,54 @@
-var express = require('express');
+var express = require("express");
 
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", {
+    title: "Express",
+    links: ["/views/css_learning", "/views/css_learning"]
+  });
 });
-
-
-
 
 /* GET object pages */
-router.get('/javascript_tuts/tuts_all_scripts_css/:subject', function(req, res) {
-    const pathViews  = req.app.locals.pathViews;
-    const subjectReq = req.params.subject;
-    res.sendFile(`${ pathViews }/javascript_tuts/${ subjectReq }/${ subjectReq }.html`);
+router.get("/javascript_tuts/tuts_all_scripts_css/:subject", function(
+  req,
+  res
+) {
+  const pathViews = req.app.locals.pathViews;
+  const subjectReq = req.params.subject;
+  res.sendFile(`${pathViews}/javascript_tuts/${subjectReq}/${subjectReq}.html`);
 });
 /* GET array pages */
-router.get('/array/:subject', function(req, res, next) {
-    const pathViews  = req.app.locals.pathViews;
-    const subjectReq = req.params.subject;
-    res.sendFile(pathViews+'/javascript_tuts/object_tuts/object_tuts.html');
+router.get("/array/:subject", function(req, res, next) {
+  const pathViews = req.app.locals.pathViews;
+  const subjectReq = req.params.subject;
+  res.sendFile(pathViews + "/javascript_tuts/object_tuts/object_tuts.html");
 });
 /* GET promise pages */
-router.get('/promise/:subject', function(req, res, next) {
-    const pathViews  = req.app.locals.pathViews;
-    const subjectReq = req.params.subject;
-    res.sendFile(pathViews+'/javascript_tuts/object_tuts/object_tuts.html');
+router.get("/promise/:subject", function(req, res, next) {
+  const pathViews = req.app.locals.pathViews;
+  const subjectReq = req.params.subject;
+  res.sendFile(pathViews + "/javascript_tuts/object_tuts/object_tuts.html");
 });
 /* GET loops pages */
-router.get('/loops/:subject', function(req, res, next) {
-    const pathViews  = req.app.locals.pathViews;
-    const subjectReq = req.params.subject;
-    res.sendFile(pathViews+'/javascript_tuts/object_tuts/object_tuts.html');
+router.get("/loops/:subject", function(req, res, next) {
+  const pathViews = req.app.locals.pathViews;
+  const subjectReq = req.params.subject;
+  res.sendFile(pathViews + "/javascript_tuts/object_tuts/object_tuts.html");
 });
 
 /* GET Fumction pages */
-router.get('/loops/:subject', function(req, res, next) {
-    const pathViews  = req.app.locals.pathViews;
-    const subjectReq = req.params.subject;
-    res.sendFile(pathViews+'/javascript_tuts/function_tuts/function_tuts.html');
+router.get("/loops/:subject", function(req, res, next) {
+  const pathViews = req.app.locals.pathViews;
+  const subjectReq = req.params.subject;
+  res.sendFile(pathViews + "/javascript_tuts/function_tuts/function_tuts.html");
 });
 /* GET String pages */
-router.get('/string/:subject', function(req, res, next) {
-    const pathViews  = req.app.locals.pathViews;
-    const subjectReq = req.params.subject;
-    res.sendFile(pathViews+'/javascript_tuts/string_tuts/string_tuts.html');
+router.get("/string/:subject", function(req, res, next) {
+  const pathViews = req.app.locals.pathViews;
+  const subjectReq = req.params.subject;
+  res.sendFile(pathViews + "/javascript_tuts/string_tuts/string_tuts.html");
 });
 
 module.exports = router;
