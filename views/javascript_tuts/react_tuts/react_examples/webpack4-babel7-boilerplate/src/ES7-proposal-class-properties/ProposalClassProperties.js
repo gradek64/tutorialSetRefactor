9a4e5d796760112d31e3bbc37c2e7464f6b72c27
gradek64 <gradek64@yahoo.com>
2020-04-29 17:45:11 +0100

    page generated with test jest
    test_jest_update
    
    ss

--- views/javascript_tuts/react_tuts/react_examples/webpack4-babel7-boilerplate/src/ES7-proposal-class-properties/ProposalClassProperties.js
+++ views/javascript_tuts/react_tuts/react_examples/webpack4-babel7-boilerplate/src/ES7-proposal-class-properties/ProposalClassProperties.js
@@ -1,17 +1,56 @@
-import React from 'react';
+import React from "react";
 
 // custom;
-import './styles.scss';
+import "./styles.scss";
 
 class Proposal extends React.Component {
-  state={
-    property: 'Proposal',
-  }
+  state = {
+    property: "Proposal",
+  };
+  myRef = React.createRef();
+
+  scrollParagraph = () => {
+    const node = this.myRef.current;
+    node.scroll({
+      left: 300,
+      behavior: "smooth",
+    });
+  };
+
+  scrollWindow = () => {
+    console.log("window", window.scrollTo);
+    window.scrollTo({
+      bottom: 1000,
+      behavior: "smooth",
+    });
+  };
 
   render() {
     const { property } = this.state;
-    console.log(property);
-    return (<span className="my-class">{`value from state is there yeah ${property}`}</span>);
+    return (
+      <>
+        <div
+          style={{
+            width: "200px",
+            border: "1px dotted green",
+            overflow: "auto",
+          }}
+          ref={this.myRef}
+        >
+          <p style={{ width: "500px" }}>
+            I will scroll when you click button scroll
+          </p>
+        </div>
+        <button
+          onClick={() => {
+            this.scrollParagraph();
+            this.scrollWindow();
+          }}
+        >
+          button scroll
+        </button>
+      </>
+    );
   }
 }
 
--- views/javascript_tuts/react_tuts/react_examples/webpack4-babel7-boilerplate/src/ES7-proposal-class-properties/ProposalClassProperties.test.js
+++ views/javascript_tuts/react_tuts/react_examples/webpack4-babel7-boilerplate/src/ES7-proposal-class-properties/ProposalClassProperties.test.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import ProposalClassProperties from './ProposalClassProperties';
+import ProposalClassProperties from "./ProposalClassProperties";
 
-test('renders correctly', () => {
+test("renders correctly", () => {
   const wrapper = shallow(<ProposalClassProperties />);
   expect(wrapper).toMatchSnapshot();
+});
+
+beforeAll(() => {
+  //spyOn window.scrollTo
+  jest.spyOn(window, "scrollTo").mockImplementation(() => {});
+});
+
+const mockRef = (instance) => {
+  //create myRef as mock
+  instance.myRef.current = document.createElement("div");
+  instance.myRef.current.scroll = jest.fn();
+  instance.scrollParagraph = jest.fn();
+  instance.scrollWindow = jest.fn();
+};
+
+fit("scrolls container after click button action", () => {
+  const wrapper = shallow(<ProposalClassProperties />);
+  const instance = wrapper.instance();
+  mockRef(instance);
+
+  const btnActionScroll = wrapper.find("button");
+  btnActionScroll.simulate("click");
+
+  //assertions
+  expect(instance.scrollParagraph).toHaveBeenCalled();
+  expect(instance.scrollWindow).toHaveBeenCalled();
 });
