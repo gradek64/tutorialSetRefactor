7aadb56dd3d5fc43fbe83c10bd6b4c13e32e2496
gradek64 <gradek64@yahoo.com>
2023-01-14 11:23:09 +0000

    start raspi pi opener App UI done

--- python_tutorials/open_apps_starter/web_client/index.js
+++ python_tutorials/open_apps_starter/web_client/index.js
@@ -1,5 +1,6 @@
 const startTimerApp = document.getElementById('startTimerApp');
 const startWaterHeaterApp = document.getElementById('startWaterHeaterApp');
+const closePieButton = document.getElementById('closePieButton');
 
 startTimerApp.addEventListener('click', async () => {
   await eel.send_data('startTimerApp');
@@ -8,3 +9,35 @@ startTimerApp.addEventListener('click', async () => {
 startWaterHeaterApp.addEventListener('click', async () => {
   await eel.send_data('startWaterHeaterApp');
 });
+
+closePieButton.addEventListener('click', async () => {
+  console.log('close App');
+  await eel.confirmClosePi();
+});
+
+/***** open overlay to close Pi all together*****/
+const overlayClosePi = document.getElementById('overlayClosePi');
+overlayClosePi.addEventListener('click', () => {
+  document.getElementById('closePie').style.display = 'block';
+});
+
+//overlay elements
+const outsideOverlay =
+  document.querySelectorAll('.overlay-flex-wrapper') || null;
+/****** close overlay ******/
+const closingElements = [outsideOverlay[0]];
+closingElements.forEach((element) => {
+  element.addEventListener('click', (e) => {
+    if (
+      e.target.classList.contains('progressBoardContent') ||
+      e.target.classList.contains('save')
+    ) {
+      if (document.querySelector('#closePie'))
+        document.querySelector('#closePie').style.display = 'none';
+      if (document.querySelector('#heatingBoard'))
+        document.querySelector('#heatingBoard').style.display = 'none';
+    }
+  });
+});
+
+/****** end of close overlay ******/
